2893bcc90c15cf0ec63bfed561b9f02ccff385b2
Luca Sesti <luca.per.registrarsi@gmail.com>
2024-11-22 09:33:54 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,61 @@
-function stampa() {
-    var print=document.getElementById("Nome").value
-    var print2=document.getElementById("Cognome").value  
-    var print3=document.getElementById("Indirizzo").value  
-    var print4=document.getElementById("Città").value  
-    var print5=document.getElementById("Email").value
+function getContatore() {
+    let contatore = localStorage.getItem("cont");
+    if (contatore === null) {
+        contatore = 1;
+    } else {
+        contatore = parseInt(contatore) + 1;
+    }
+    localStorage.setItem("cont", contatore);
+    return contatore;
+}
 
-    var table = document.getElementById("tabella");
-    var row = table.insertRow(-1);
-    var cell1 = row.insertCell(0);
-    var cell2 = row.insertCell(1);
-    var cell3 = row.insertCell(2);
-    var cell4 = row.insertCell(3);
-    var cell5 = row.insertCell(4);
-    cell1.innerHTML = print;
-    cell2.innerHTML = print2;
-    cell3.innerHTML = print3;
-    cell4.innerHTML = print4;
-    cell5.innerHTML = print5;
-  }
+function stampa() {
+    const nuovaRiga = [];
+    let print = document.getElementById("Nome").value;
+    let print2 = document.getElementById("Cognome").value;  
+    let print3 = document.getElementById("Indirizzo").value;  
+    let print4 = document.getElementById("Città").value;  
+    let print5 = document.getElementById("Email").value;
+
+    nuovaRiga.push(print, print2, print3, print4, print5);
+    const i = getContatore();
+    localStorage.setItem("Info" + i, nuovaRiga);
+}
+
+function aggiornaTabella() {
+    let table = document.getElementById("tabella");
+    let i = 1;
+    while (localStorage.getItem("Info" + i) !== null) {
+        let infoTemp = localStorage.getItem("Info" + i);
+        const info = infoTemp.split(",");
+
+        let row = table.insertRow(-1);
+        let cell1 = row.insertCell(0);
+        let cell2 = row.insertCell(1);
+        let cell3 = row.insertCell(2);
+        let cell4 = row.insertCell(3);
+        let cell5 = row.insertCell(4);
+
+        cell1.innerHTML = info[0];
+        cell2.innerHTML = info[1];
+        cell3.innerHTML = info[2];
+        cell4.innerHTML = info[3];
+        cell5.innerHTML = info[4];
+
+        i++;
+    }
+}
+
+function svuotaTabella() {
+    let table = document.getElementById("tabella");
+    let contRighe = table.rows.length;
+    for (let i = contRighe - 1; i > 0; i--) {
+        table.deleteRow(i);
+    }
+    let i = 1;
+    while (localStorage.getItem("Info" + i) !== null) {
+        localStorage.removeItem("Info" + i);
+        i++;
+    }
+    localStorage.removeItem("cont");
+}
