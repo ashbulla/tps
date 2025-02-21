a01ae2b39ae865464a65802bf1676c29de61861d
Luca Sesti <luca.per.registrarsi@gmail.com>
2025-02-21 11:01:56 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,52 @@
-const canvas = document.getElementById("Canvas")
-const ctx = canvas.getContext("2d")
+let i = 0;
+let tabella = [];
 
-ctx.moveTo(240, 75);
-ctx.lineTo(345, 390);
-ctx.lineTo(71,205);
-ctx.lineTo(409,205);
-ctx.lineTo(133, 390);
-ctx.lineTo(240, 75);
-ctx.moveTo(415,250);
-ctx.arc(240, 250, 175, 0, 2 * Math.PI);
-ctx.stroke();
+function salvaDati() {
+    const dati = {
+        nome: document.getElementById("nome").value,
+        cognome: document.getElementById("cognome").value,
+        citta: document.getElementById("citta").value,
+        indirizzo: document.getElementById("indirizzo").value,
+        mail: document.getElementById("mail").value
+    };
+
+    tabella.push(dati);
+    localStorage.setItem("tabella", JSON.stringify(tabella));
+
+    document.getElementById("nome").value = "";
+    document.getElementById("cognome").value = "";
+    document.getElementById("citta").value = "";
+    document.getElementById("indirizzo").value = "";
+    document.getElementById("mail").value = "";
+
+    inserisciDati();
+}
+
+function inserisciDati() {
+    let tab = document.getElementById("tabella");
+    tab.innerHTML = "";  
+    let t = JSON.parse(localStorage.getItem("tabella"));
+
+    for (let z = 0; z < t.length; z++) {
+        let utente = t[z];
+
+        let nuovaRiga = tab.insertRow();
+        let contatore = nuovaRiga.insertCell(0);
+        contatore.innerHTML = z + 1;
+
+        let nome = nuovaRiga.insertCell(1);
+        nome.innerHTML = utente.nome;
+        
+        let cognome = nuovaRiga.insertCell(2);
+        cognome.innerHTML = utente.cognome;
+
+        let citta = nuovaRiga.insertCell(3);
+        citta.innerHTML = utente.citta;
+
+        let indirizzo = nuovaRiga.insertCell(4);
+        indirizzo.innerHTML = utente.indirizzo;
+
+        let mail = nuovaRiga.insertCell(5);
+        mail.innerHTML = utente.mail;
+    }
+}
